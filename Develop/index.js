91de0bd2df899fd6cb3cb6b3e16b9b93bcac9f19
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-12-18 14:18:24 -0500

    Created question prompts for the index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,84 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+        validate: titleInput => titleInput ? true : 'Title is required!',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'githubUsername',
+        validate: githubInput => githubInput ? true : 'Username is required!',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+        validate: emailInput => emailInput ? true : 'Email is required!',
+    },
+    {
+        type: 'input',
+        message: 'What problem will your project solve?',
+        name: 'what',
+        validate: whatInput => whatInput ? true : 'Description of what your project will solve is required!',
+    },
+    {
+        type: 'input',
+        message: 'Why did you build this project?',
+        name: 'why',
+        validate: whyInput => whyInput ? true : 'A reason for building this project is required!',
+    },
+    {
+        type: 'input',
+        message: 'What did you learn in building this project?',
+        name: 'learn',
+        validate: learnInput => learnInput ? true : 'What you learned is required!',
+    },
+    {
+        type: 'input',
+        message: 'How will someone use this project?',
+        name: 'how',
+        validate: howInput => howInput ? true : 'How this project is used is required!',
+    },
+    {
+        type: 'input',
+        message: 'Provide installation instructions for this project.',
+        name: 'installation',
+        validate: installInput => installInput ? true : 'Installation instructions are required!',
+    },
+    {
+        type: 'list',
+        message: 'What license would you like to use for your project?',
+        name: 'license',
+        choices: ['agpl', 'apache', 'MIT', 'no license']
+    },
+    {
+        type: 'confirm',
+        message: 'Would you like to let other developers contribute to you project?',
+        name: 'confirmContributers',
+        default: true
+    },
+    {
+        type: 'input',
+        message: 'Provide information on how to contribute.',
+        name: 'contribute',
+        when: ({confirmContributers}) => confirmContributers,
+        validate: contributerInput => contributerInput ? true : 'Information on contributing is required!'
+    },
+    {
+        type: 'input',
+        message: 'How can this app be tested?',
+        name: 'test',
+        validate: testInput => testInput ? true : 'Test instructions are required!',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
