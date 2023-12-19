527422227d009505ae284121c83e13e72b6d40fa
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-12-19 16:19:24 -0500

    Created functions for the generateMarkdowns.js file and ran the program to check for errors

---
+++ Develop/output/generated-README.md
@@ -0,0 +1,41 @@
+
+  
+  # README Generator
+
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Why](#why)
+  - [What I Learned](#what-i-learned)
+  - [How to Use](#how-to-use)
+  - [Installation](#installation)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Description
+  Makes creating a README easier
+  To simplify the creation process of a README file
+  They will download it and enter the answers of the questions for the README in the terminal
+
+  ## What I Learned
+  I learned how to import and export files, work with node.js, object deconstruction, the use of template literals, let and     
+
+  ## Installation
+  installation
+
+  ## License
+  [Apache license 2.0](https://opensource.org/licenses/Apache-2.0)
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+  ##Contributing
+  contribute
+
+  ## Tests
+  test
+
+  ## Questions 
+  - GitHub: [KyleGru](https://github.com/KyleGru)
+  - Email: kylegruschow@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,103 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'agpl':
+      return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+    case 'apache': 
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    case 'MIT':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case 'no license':
+      return `No license`;
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'agpl':
+      return `[GNU AGPL v3](https://www.gnu.org/licenses/agpl-3.0)`;
+    case 'apache': 
+      return `[Apache license 2.0](https://opensource.org/licenses/Apache-2.0)`;
+    case 'MIT':
+      return `[MIT license](https://opensource.org/licenses/MIT)`;
+    case 'no license':
+      return `No license`;
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'agpl':
+      return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+    case 'apache': 
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    case 'MIT':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case 'no license':
+      return `No license`;
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  
+  const { title, githubUsername, email, what, why, learn, how, installation, license, confirmContributers, contribute, test } = data;
+  const licenseBadge = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
+  const licenseSection = renderLicenseSection(license);
 
+  return `
+  
+  # ${title}
+
+  ${licenseBadge}
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Why](#why)
+  - [What I Learned](#what-i-learned)
+  - [How to Use](#how-to-use)
+  - [Installation](#installation)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Description
+  ${what}
+  ${why}
+  ${how}
+
+  ## What I Learned
+  ${learn}
+
+  ## Installation
+  ${installation}
+
+  ## License
+  ${licenseLink}
+  ${licenseSection}
+
+  ##Contributing
+  ${confirmContributers ? contribute : 'Contributions are not accepted at the moment'}
+
+  ## Tests
+  ${test}
+
+  ## Questions 
+  - GitHub: [${githubUsername}](https://github.com/${githubUsername})
+  - Email: ${email}
 `;
 }
 
